37e6dd101eda3fad1d30eb9ffd08300f16539c7a
jojima <fabio_jojima@hotmail.com>
2016-12-19 21:59:09 -0200

    Alteracoes no zipzop.py

--- t1-im/src/ZipZop.py
+++ t1-im/src/ZipZop.py
@@ -4,12 +4,15 @@ from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.config import Config
 import client
+import kivy.clock
 from time import *
-
+import os
 client = client.Client()
 HOST_ADDRESS = "localhost"
 PORT = 8003
 aux = 1
+username = ""
+receiver = ""
 
 class Login(Screen):
     # Configuring login screen size
@@ -20,6 +23,8 @@ class Login(Screen):
     def sucess(self,loginTxt):
         app = App.get_running_app()
         app.username = loginTxt
+        global username
+        username = loginTxt
         global client
         client.start(HOST_ADDRESS, PORT, loginTxt)
 
@@ -36,12 +41,32 @@ class Login(Screen):
 class Connected(Screen):
     global aux
     def newConnection(self,loginTxt):
+        clock.schedule_interval(rcv_msg,0.5)
         if aux < 9 :
             current_user = "User"+str(aux)
             self.ids[current_user].text = loginTxt
             self.ids[current_user].background_color = 1.0,1.0,1.0,1.0
             global aux
             aux += 1
+
+    def rcv_msg(self):
+        global client
+        if len(client.recv_list):
+            for i in client.recv_list:
+                i += "\n"
+                dir_path = os.getcwd()
+                global username
+                filename = dir_path+username+";"
+                target = i[1]
+                counter = 2
+                while i[counter] != "]":
+                    target += i[counter]
+                    counter += 1
+                filename += target
+                log = open(filename,'a')
+                log.write(i)
+                log.close()
+                #  printa na tela
 
     def disconnect(self):
         self.manager.transition = SlideTransition(direction="right")
@@ -50,10 +75,18 @@ class Connected(Screen):
 
     def send(self):
         message = self.ids['message'].text
+        global receiver
+        receiver
         global client
-        client.send_message(['joao'], message)
+        client.send_message([receiver], message)
         self.ids['message'].text = ""
         sleep(1)
+        dir_path = os.getcwd()
+        filename = dir_path+username+";"+receiver
+        log = open(filename,'a')
+        message += "\n"
+        log.write(message)
+        log.close()
         global client
         #self.ids['log'].text = client.rcv_list[0][1]
 
